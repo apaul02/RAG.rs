3fe45c415ab605bac7c111a632a4dd9418a1d22c
apaul02 <captainankan@gmail.com>
2026-04-23 17:24:11 +0530

    embeddings

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod chunker;
+pub mod embedder;
 pub mod models;
 
 use crate::chunker::ChunkContext;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut chunker = ChunkContext::new();
     chunker.process_file("distributed.txt");
     for l in chunker.finished_chunk {
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,4 @@
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 pub enum ParserState {
     NormalText,
@@ -9,6 +10,32 @@ pub struct Chunk {
     pub chunk_id: String,
     pub file_name: String,
     pub text: String,
+    pub embeddings: Option<Vec<f32>>,
+}
+
+#[derive(Serialize)]
+pub struct Part {
+    pub text: String,
+}
+
+#[derive(Serialize)]
+pub struct Content {
+    pub parts: Vec<Part>,
+}
+
+#[derive(Serialize)]
+pub struct GeminiRequest {
+    pub content: Content,
+}
+
+#[derive(Deserialize)]
+pub struct Embeddings {
+    pub values: Vec<f32>,
+}
+
+#[derive(Deserialize)]
+pub struct GeminiResponse {
+    pub embedding: Embeddings,
 }
 
 impl Chunk {
@@ -17,6 +44,7 @@ impl Chunk {
             chunk_id: Uuid::new_v4().to_string(),
             file_name,
             text,
+            embeddings: None,
         }
     }
 }
